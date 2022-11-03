8fa1e4744fe260eec15138dfd388071fd7892845
Cheng Lim <wenyong.lim@icloud.com>
2022-11-03 21:54:24 +0000

    btn click event listener

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
-  const onClick = () => {
-    console.log('click');
-  };
+const Button = ({ color, text, onClick }) => {
   return (
     <button
       onClick={onClick}
@@ -22,6 +19,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e);
+  };
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Hello' />
+      <Button color='green' text='Hello' onClick={onClick} />
     </header>
   );
 };
