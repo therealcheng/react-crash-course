bb48a1644525eb1f115c171071a9da7b1c213c41
Cheng Lim <wenyong.lim@icloud.com>
2022-11-03 20:44:30 +0000

    app changing jsx to show h1

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+// * everything we see in the app is from here.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello From React!</h1>
     </div>
   );
 }
 
 export default App;
+
+// ! Notes
+/*
+className = css class
+for app = htmlFor
+*/
