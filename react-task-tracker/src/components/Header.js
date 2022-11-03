ce46de70b3222081048cfda0e58be03cf38a28b8
Cheng Lim <wenyong.lim@icloud.com>
2022-11-03 21:18:05 +0000

    def prop and prop-types

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Task tracker</h1>
+      <h1>{title}</h1>
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: 'Task tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
