b7064faf4f33f687e24dc1b6e1eb9c820911ed6d
Cheng Lim <wenyong.lim@icloud.com>
2022-11-04 22:05:13 +0000

    added styling to TaskForm

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -48,6 +49,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
