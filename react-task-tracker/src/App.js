56553ba06a8838c243710ce4e777e55183d36846
Cheng Lim <wenyong.lim@icloud.com>
2022-11-04 20:27:46 +0000

    delete task function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -33,13 +33,13 @@ function App() {
 
   // Delete task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ''}
     </div>
   );
 }
