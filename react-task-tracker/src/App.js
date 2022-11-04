261c3600f1086ee1379f92dd2942bb95c830e5ec
Cheng Lim <wenyong.lim@icloud.com>
2022-11-04 16:54:18 +0000

    refactoring task prop to be in app

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,10 +4,36 @@ import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor',
+      day: 'Feb 5th at 3:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Library',
+      day: 'Feb 5th at 3:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Bank',
+      day: 'Feb 5th at 3:30pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Super Market',
+      day: 'Feb 5th at 3:30pm',
+      reminder: true,
+    },
+  ]);
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,32 +1,4 @@
-import { useState } from 'react';
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctor',
-      day: 'Feb 5th at 3:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Library',
-      day: 'Feb 5th at 3:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Bank',
-      day: 'Feb 5th at 3:30pm',
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: 'Super Market',
-      day: 'Feb 5th at 3:30pm',
-      reminder: true,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
   return (
     <>
       {' '}
