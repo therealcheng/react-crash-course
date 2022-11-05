d3521dc1e9f5225c8580533a2f1ea00f8df452ca
Cheng Lim <wenyong.lim@icloud.com>
2022-11-05 19:51:36 +0000

    form input state

--- react-task-tracker/src/components/AddTask.js
+++ react-task-tracker/src/components/AddTask.js
@@ -1,17 +1,37 @@
+import { useState } from 'react';
+
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label>Task</label>
-        <input type='text' placeholder='Add task' />
+        <input
+          type='text'
+          placeholder='Add task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className='form-control'>
         <label>Day & Time</label>
-        <input type='text' placeholder='Add day and time' />
+        <input
+          type='text'
+          placeholder='Add day and time'
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className='form-control form-control-check'>
         <label>Set Reminder</label>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          value={reminder}
+          onChange={(e) => setReminder(e.target.checked)}
+        />
       </div>
 
       <input type='submit' value='Save Task' className='btn btn-block' />
