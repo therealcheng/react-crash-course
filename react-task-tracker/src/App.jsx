ccaa5cd89a41ed54f9157bbf739eeec48eecc1da
Cheng Lim <wenyong.lim@icloud.com>
2022-11-06 21:26:48 +0000

    toggle reminder on server

--- react-task-tracker/src/App.jsx
+++ react-task-tracker/src/App.jsx
@@ -25,6 +25,14 @@ function App() {
     return data;
   };
 
+  // Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
@@ -51,10 +59,23 @@ function App() {
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.toggleReminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
