46203c3e89788c0ea8c170d09f46797e8b03e3f8
Cheng Lim <wenyong.lim@icloud.com>
2022-11-06 21:16:41 +0000

    add task with useEffect async

--- react-task-tracker/src/App.jsx
+++ react-task-tracker/src/App.jsx
@@ -26,11 +26,20 @@ function App() {
   };
 
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    console.log(id);
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // console.log(id);
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   // Delete task
