e558a08420bb24694a1e29a667b571eb0421f4fd
Cheng Lim <wenyong.lim@icloud.com>
2022-11-06 17:09:56 +0000

    Add button toggle on click - show and hide form

--- react-task-tracker/src/components/Header.jsx
+++ react-task-tracker/src/components/Header.jsx
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-  const onClick = (e) => {
-    console.log('click');
-  };
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Hello' onClick={onClick} />
+      <Button color='green' text='Add' onClick={onAdd} />
     </header>
   );
 };
