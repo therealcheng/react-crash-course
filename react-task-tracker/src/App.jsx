4680ffb4d90711332a184a8de4892a9ebf9bae33
Cheng Lim <wenyong.lim@icloud.com>
2022-11-06 17:06:48 +0000

    showAddTask state

--- react-task-tracker/src/App.jsx
+++ react-task-tracker/src/App.jsx
@@ -5,6 +5,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -57,7 +58,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask onAdd={addTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
